3e72323bb430fce0ee4d6866e832b6a9b5f7234d
allen-8 <90605398+allen-8@users.noreply.github.com>
2023-01-24 23:35:13 +0100

    Update InitController.java

--- pizza-shop/src/main/java/com/project/pizzashop/controller/InitController.java
+++ pizza-shop/src/main/java/com/project/pizzashop/controller/InitController.java
@@ -26,36 +26,46 @@ public class InitController implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-                orders.deleteAll();
+        orders.deleteAll();
         users.save(new User(1, "Admin", "", "admin@pizza.com", false, "Passw0rd!"));
         Restaurant citypizza = new Restaurant("City Pizza", "Berlin", "Pizzastr.1, 12345", "citypizza@pizza.com",
-                "+49123456789", 4.6, "https://www.alytusinfo.lt/data/tourism_objects/large/city_pizza_alytus__1_.jpg");
+                "+49123456789", 4.6, "https://www.alytusinfo.lt/data/tourism_objects/large/city_pizza_alytus__1_.jpg");;
         Restaurant dominos = new Restaurant("Dominos Pizza", "Berlin", "Pizzastr.2, 67890", "dominos@pizza.com",
                 "", 3.8, "https://lh5.googleusercontent.com/p/AF1QipOTtUTZ6Vs0_0XUfg2SLO2gjH61TulPHI8AF_Fj=w408-h306-k-no");
         Restaurant allora = new Restaurant("Pizerria Allora", "Ulm", "Ulmstr.1, 34567", "allora@pizza.com",
                 "+49987654321", 4.1, "https://lh5.googleusercontent.com/p/AF1QipPWKWtPIDKs6g6SqId7v_ZIPCBIFaIpsMtyIu2N=w408-h544-k-no");
-        restaurants.save(citypizza);
-        restaurants.save(dominos);
-        restaurants.save(allora);
-        pizzas.save(new Pizza("Carbonara", citypizza, "Delicious pizza with cheese, mashrooms, onions & carbonara sause",
+        if (restaurants.findByName("City Pizza").isEmpty())
+            restaurants.save(citypizza);
+        if (restaurants.findByName("Dominos Pizza").isEmpty())
+            restaurants.save(dominos);
+        if (restaurants.findByName("Pizerria Allora").isEmpty())
+            restaurants.save(allora);
+        if (pizzas.findByName("Carbonara").isEmpty())
+            pizzas.save(new Pizza("Carbonara", restaurants.findByName("City Pizza").get(), "Delicious pizza with cheese, mashrooms, onions & carbonara sause",
                 5.6, 7.8, 12.3,
                 "https://img.taste.com.au/6MQTA3rp/taste/2016/11/carbonara-pizza-our-most-recent-recipes-of-the-day-104195-2.jpeg"));
-        pizzas.save(new Pizza("Marinara", citypizza, "Tomato Sauce, Garlic, Oregano & Basil. Vegan.",
+        if (pizzas.findByName("Marinara").isEmpty())
+            pizzas.save(new Pizza("Marinara", restaurants.findByName("City Pizza").get(), "Tomato Sauce, Garlic, Oregano & Basil. Vegan.",
                 4.3, 6.2, 10.6,
                 "https://flipdish.imgix.net/m10fmGwpZOWRKbrntJ4IuDhW7D0.jpg?auto=format&auto=compress&fit=crop&w=767&h=460&dpr=1"));
-        pizzas.save(new Pizza("Anchovies", citypizza, "Anchovies, Fresh Chilies, Garlic, Capers, Mozzarella, Parsley & Tomato Sauce.",
+        if (pizzas.findByName("Anchovies").isEmpty())
+            pizzas.save(new Pizza("Anchovies", restaurants.findByName("City Pizza").get(), "Anchovies, Fresh Chilies, Garlic, Capers, Mozzarella, Parsley & Tomato Sauce.",
                 -1, 6.8, -1,
                 "https://flipdish.imgix.net/XX17jEFwVvjLYaLvmAHO8Vw3Gl4.jpg?auto=format&auto=compress&fit=crop&w=767&h=460&dpr=1"));
-        pizzas.save(new Pizza("Margarita", dominos, "Mozzarella, Parmesan, Tomato Sauce, Olive Oil & Basil.",
+        if (pizzas.findByName("Margarita").isEmpty())
+            pizzas.save(new Pizza("Margarita", restaurants.findByName("Dominos Pizza").get(), "Mozzarella, Parmesan, Tomato Sauce, Olive Oil & Basil.",
                 4.6, 6.9, 11.3,
                 "https://flipdish.imgix.net/cOpSOzJIDhRn0xJk10gIaDtfMc.jpg?auto=format&auto=compress&fit=crop&w=767&h=460&dpr=1"));
-        pizzas.save(new Pizza("Salami", dominos, "",
+        if (pizzas.findByName("Salami").isEmpty())
+            pizzas.save(new Pizza("Salami", restaurants.findByName("Dominos Pizza").get(), "",
                 4.6, -1, 11.3,
                 "https://imageproxy.wolt.com/menu/menu-images/620d5472a798a099bffed460/74bd5538-909e-11ec-9727-122789fc7683_02_pizza_salami.jpeg"));
-        pizzas.save(new Pizza("Prosciutto", allora, "With cheese and ham",
+        if (pizzas.findByName("Prosciutto").isEmpty())
+            pizzas.save(new Pizza("Prosciutto", restaurants.findByName("Pizerria Allora").get(), "With cheese and ham",
                 -1, 7.1, 10.2,
                 "https://imageproxy.wolt.com/menu/menu-images/620d5472a798a099bffed460/7bc8149e-909e-11ec-8476-22b1de579f77_04_pizza_prosciutto.jpeg"));
-        pizzas.save(new Pizza("Pizza Chicken", allora, "with cheese, chicken, onions & corn",
+        if (pizzas.findByName("Pizza Chicken").isEmpty())
+            pizzas.save(new Pizza("Pizza Chicken", restaurants.findByName("Pizerria Allora").get(), "with cheese, chicken, onions & corn",
                 5.2, 7.8, 10.9,
                 "https://imageproxy.wolt.com/menu/menu-images/620d5472a798a099bffed460/8b41f750-909e-11ec-a239-ba447eadc14f_24_pizza_chicken.jpeg"));
     }
